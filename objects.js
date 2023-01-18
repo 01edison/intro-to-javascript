bae45543736e071c2bed6e99f3f4a066323431a8
MGBEOKWERE CHIMDINDU <cmgbeokwere6@gmail.com>
2023-01-18 14:57:13 +0100

    second lesson

--- objects.js
+++ objects.js
@@ -3,6 +3,34 @@ let x = 5  // the "let" keyword allows you to create variables that you can re-a
 
 const y = 6 // the "const" keyword allow you to create variables that cannot be changed later
 
+//ARITHMETIC OPERATORS
+// + - * / % **
+// const num = prompt("type in a number")
+// if (num % 2 == 0) {
+//     console.log("even")
+// } else {
+//     console.log("odd")
+// }
+
+
+//COMPARISON OPERATORS
+
+// == != > < <= >=
+
+
+//LOGICAL OPERATORS
+// &&(AND) ||(OR) !(NOT)
+
+// const age = prompt("type in your age")
+
+// if (age < 13) {
+//     console.log("child")
+// } else if (age >= 13 && age < 20) {
+//     console.log("teenager")
+// } else {
+//     console.log("adult")
+// }
+
 //AREA OF A CIRCLE
 // const pi = 3.142;
 
@@ -20,26 +48,42 @@ const y = 6 // the "const" keyword allow you to create variables that cannot be 
 // console.log(typeof converted)
 // console.log(converted)
 
-// const age = prompt("Type in a number")
+// const time = prompt("What is the time?")
 
-// if (age < 13) {
-//     console.log("pikin")
-// } else if (age < 20) {
-//     console.log("teenager")
-//     return
+// if (time < 12) {
+//     console.log("good morning")
+// } else if (time < 18) {
+//     console.log("good afternoon")
+// } else if (time < 23) {
+//     console.log("good evening")
 // } else {
-//     console.log("adult")
+//     console.log("enter a valid time input")
 // }
 
-function ageChecker(age) {
-    if (age < 13) {
-        console.log("pikin")
-    } else if (age < 20) {
-        console.log("teenager")
-        return age;
-    } else {
-        console.log("adult")
-    }
-}
+// const score = prompt("please enter a score")
 
-const age = ageChecker(13)
+// if (score >= 70 && score < 100) {
+//     console.log("A")
+// }
+// else if (score >= 60 && score < 70) {
+//     console.log("B")
+// } else if (score >= 50 && score < 60) {
+//     console.log("C")
+// } else if (score >= 45 && score < 50) {
+//     console.log("D")
+// } else if (score > 0 && score < 45) {
+//     console.log("F")
+// } else {
+//     console.log("invalid")
+// }
+
+
+//STRINGS
+
+const str = "hello there today is wednesday"
+
+// const arr = ["a", "b", "c", 1, 2]
+
+console.log(arr.length)
+console.log(str.split(","))
+//
